d52c0905c59034a1d941341fb1ef3a9aa8e90192
Sveta <apples-time@ya.ru>
2018-05-28 19:32:39 +0300

    modified

--- subagents.sql
+++ subagents.sql
@@ -12,7 +12,7 @@ SELECT Ag.NAME
 	,(B.Summa - B.SummaCom +( DSC.ComPart)) as Netto
 	,P.Paid
 	--,CAST(P.RUB AS INT) AS Rub
-	,CAST((BP.EDate - 1) AS DATE) AS PaymentDate
+	, P.MaxPay
 
 FROM [dbo].[BOrders] AS BO
 LEFT OUTER JOIN Profiles AS PR ON BO.ManagerID = PR.UserID
@@ -22,12 +22,14 @@ JOIN (
 	SELECT [BOrderID]
 		,SUM(BPI.Amount) AS Paid
 		--,SUM(BPI.Amount * BPI.Rate) AS RUB
-		,COUNT(BP.Pos) AS MaxPay
+		--,COUNT(BP.Pos) AS MaxPay
+		, MAX(BP.EDate) as MaxPay
 	FROM [dbo].[BPayments] AS BP
 	JOIN BPaymentItems AS BPI ON BP.BPaymentID = BPI.BPaymentID
 	WHERE BP.Status = 200
 	GROUP BY BOrderID
 	) AS P ON BO.BOrderID = P.BOrderID
+
 LEFT JOIN BCruises BC ON BC.BOrderID = BO.BOrderID
 JOIN (
 	SELECT   SUM(CAST(FLOOR((BCP.p - BCP.ncf)*BC.Commission/100)as INT)) AS SummaCom
@@ -50,14 +52,14 @@ JOIN (
 	JOIN BItemPersons AS BIP ON BIP.BItemID = BI.BItemID
 	GROUP BY BOrderID
 	) AS DSC ON DSC.BOrderID = BO.BOrderID
-JOIN BPayments AS BP ON BP.BOrderID = BO.BOrderID
+--JOIN BPayments AS BP ON BP.BOrderID = BO.BOrderID
 
 WHERE BO.StatusID >= 100
 	AND (B.Summa -B.SummaCom + DSC.ComPart) = P.Paid
 	AND BO.AgencyID IS NOT NULL 
 	AND BO.AgencyID NOT IN ('4d318ee4-d8a2-4806-860c-d21dd8733e56','aa1f5f88-95c6-44d1-ab0b-8d43f666e9b3')
-	AND BP.Pos = MaxPay
-	AND (BP.EDate > '2018-04-01' AND BP.EDate <= '2018-05-01') 
+	--AND BP.Pos = MaxPay
+	AND (P.MaxPay >= '2018-05-01' AND P.MaxPay <= '2018-05-30') 
 	
-ORDER BY BP.EDate
+ORDER BY P.MaxPay
 GO
