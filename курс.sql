20a84111f3c629b2e48712821ab104ad3abdadeb
Sveta <apples-time@ya.ru>
2018-08-30 18:57:19 +0300

    modified

--- курс.sql
+++ курс.sql
@@ -2,25 +2,27 @@ USE [u183189]
 GO
 
 SELECT BO.BOrderID
-      ,BO.CRMID    
+		--,BO.AgencyID
+      ,BO.CRMID  
+	 -- ,LEN(BO.CRMID)  
       ,BP.EDate
 	  ,BP.Status
 	  ,BPI.Amount
 	  ,BPI.Currency
 	  ,BPI.Rate
 	  ,BPI.Amount*BPI.Rate as Rub
+	  ,BC.CompanyID
 	  ,BP.Created
 	  ,BP.Updated
      
   FROM [dbo].[BOrders] as BO
-  JOIN BCruises as BC ON BO.BOrderID = BC.BOrderID
-  LEFT JOIN BPayments AS BP ON BO.BOrderID = BP.BOrderID
-  LEFT JOIN BPaymentItems AS BPI ON BP.BPaymentID = BPI.BPaymentID
-  WHERE BP.Created >= '2018-08-23' and BP.Status = 200  AND BO.AgencyID<>'150aec84-7a0f-49b1-97e0-ff805e5a2201'
-  AND BPI.Currency = 'EUR'
- -- AND BC.CompanyID <> 1
-
-  Order by Created
+  JOIN BCruises as BC ON BC.BOrderID = BO.BOrderID
+  LEFT JOIN BPayments AS BP ON BP.BOrderID = BO.BOrderID
+  LEFT JOIN BPaymentItems AS BPI ON BPI.BPaymentID = BP.BPaymentID
+  WHERE BP.Created >= '2018-08-08' and BP.Status = 200 AND (BO.AgencyID <> '150aec84-7a0f-49b1-97e0-ff805e5a2201' OR BO.AgencyID is NULL)
+	AND BPI.Currency = 'EUR'
+	AND BC.CompanyID <> 1
+	AND LEN(BO.CRMID) < 7
+  Order by BP.Created
 GO
-
 
